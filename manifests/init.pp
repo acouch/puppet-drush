4c3cceedfe4e188c9d0cc780ab9b60f1491ce459
JV Clemence <james@jvc26.org>
2011-11-28 22:37:09 +0000

    Added useful comments at the top of init and params

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,26 @@
+# Class: drush
+#
+# This class installs prerequisite packages and Drush. Supported on Debian
+# based systems. Module should be installed on the Puppet modules path.
+#
+# == Requires:
+#
+#   Packages: git, php5-cli, php-pear
+#   Pear Resources: Console_Table (prerequisite for Drush)
+#
+# == Variables:
+#
+#   Inherits $branch_name from drush::params
+#
+# == Examples
+#
+#   class {'drush': }
+#   include drush
+#
+# == Authors
+#
+#   James Clemence <james+github@jvc26.org>
+#
 class drush {
 
     include drush::params
