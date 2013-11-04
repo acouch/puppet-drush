f1491fe3c04b528efb610e20f5e67c95a8c09466
acouch <acinternets@gmail.com>
2013-11-03 18:52:28 -0600

    moving to vcs repo for drush

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,51 @@
-class drush ($branch = "7.x-5.x") {
+class drush {
 
-    package { 'Console_Table':
-        ensure   => present,
-        provider => pear,
-        require  => Package['php53'],
-    }
+  package { "php5-cli":
+  	ensure => installed,
+  }
 
-    exec { 'fetch-drush':
-        cwd     => '/usr/local/lib',
-        command => "/usr/bin/git clone --branch $branch http://git.drupal.org/project/drush.git",
-        # Won't run if exists: http://www.puppetcookbook.com/posts/run-exec-if-file-absent.html.
-        creates => '/usr/local/lib/drush',
-        require => Package['php53', 'Console_Table'], 
-    }
-     
-    file { '/usr/local/bin/drush':
-        ensure  => link, 
-        target => '/usr/local/lib/drush/drush',
-    }
+  vcsrepo { "/var/lib/drush":
+    ensure => present,
+    provider => git,
+    source => "https://github.com/drush-ops/drush.git",
+    revision => 'd5d09f5104b46c97e4eca50c69b8f1674ab06050',
+  }
+
+  file { "/var/lib/drush/lib/Console_Table-1.1.3":
+    require => Vcsrepo["/var/lib/drush"],
+    recurse => true,
+    source => "puppet:///modules/drush/Console_Table-1.1.3",
+  }
+
+  file { "/usr/local/bin/drush":
+    require => [Vcsrepo["/var/lib/drush"], File["/var/lib/drush/lib/Console_Table-1.1.3"]],
+    ensure => link,
+    target => "/var/lib/drush/drush",
+    owner => 'root',
+    group => 'root',
+    mode => 755,
+  }
+
+  file { "/usr/share/drush":
+    ensure => directory,
+    owner => root,
+    group => root,
+    mode => 755,
+  }
+
+  file {  "/usr/share/drush/commands":
+    require => File['/usr/share/drush'],
+    ensure => directory,
+    owner => root,
+    group => root,
+    mode => 755,
+  }
+
+  file { '/usr/local/bin/drupal-perms':
+    source => "puppet:///modules/drush/drupal-perms",
+    owner => 'root',
+    group => 'root',
+    mode => 755,
+  }
+
 }
